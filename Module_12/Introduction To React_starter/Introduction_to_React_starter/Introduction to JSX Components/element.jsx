b72f4857cabf19ea4a23035e0fbd824e45807201
Jon Malloy <jonathan.malloy3@gmail.com>
2021-12-06 11:22:17 -0500

    Through Module 14

--- Module_12/Introduction To React_starter/Introduction_to_React_starter/Introduction to JSX Components/element.jsx
+++ Module_12/Introduction To React_starter/Introduction_to_React_starter/Introduction to JSX Components/element.jsx
@@ -1,4 +1,9 @@
-const name = 'Mary';
-const element = <h1> Hello {name} </h1>;
+const name = 'Billy';
+const Element = <h1> Hello {name} </h1>;
+const Newelement = (props) => {
+    const lady = props.lady
+    return (<h2> and this is {lady} she is also in the movie</h2>)
+}
 
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(Element, document.getElementById('root')); //getting overwritten for some reason? Will try to fix this later
+ReactDOM.render(<Newelement lady = "Sally Hotstuff"/>, document.getElementById('root'));
